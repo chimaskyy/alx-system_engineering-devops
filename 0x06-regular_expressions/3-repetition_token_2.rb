074edcc5e4a018a01183c39c63eb9b3d821b56d9
chimaskyy <chiamakahj@gmail.com>
2023-10-31 21:22:49 +0100

    trying 3&4 again

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
-## matches hb followed by 1 or 5 t, followed by n
-## hbtn, hbttn, hbtttn
+# matches hb followed by 1 or 5 t, followed by n
+# hbtn, hbttn, hbtttn
 
 regex = /hbt+n/
 puts ARGV[0].scan(regex).join
