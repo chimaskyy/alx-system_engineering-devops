04a8cdd3324bea2b85374df1fdbd42ce3555e1cf
chimaskyy <chiamakahj@gmail.com>
2023-12-27 16:11:25 +0100

    client configuration using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
-# client SSH configuration file so that you can connect to a server without typing a password
+# set up client SSH configuration file
 
 #define file content
 $file_content = @("EOF")
@@ -8,11 +8,9 @@ EOF
 
 #file path and permission
 $file_path = '/etc/ssh/ssh_config'
-$file_mode = '0644'
 
 #manage file
 file { $file_path:
   ensure  => present,
-  mode    => $file_mode,
   content => $file_content,
 }
