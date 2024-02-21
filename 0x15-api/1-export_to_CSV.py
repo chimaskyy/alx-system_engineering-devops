a24147220730e8d40fac7c8f80a69aa9f5d84e46
chimaskyy <chiamakahj@gmail.com>
2024-02-21 03:36:41 +0100

    export data to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,7 +2,7 @@
 """
 This module uses the REST API of JSONPlaceholder
 of a givin employee ID, returns his/her TODO
-list progress
+list progress and write into csv file
 """
 
 import requests
@@ -17,20 +17,13 @@ def todo():
         user = requests.get(f"{url_}/users/{user_id}").json()
         tasks = requests.get(f"{url_}/users/{user_id}/todos").json()
 
-        emp_name = user.get('name')
-        num_task = len(tasks)
-        task_done = 0
-
-        for task_ in tasks:
-            if task_["completed"] is True:
-                task_done += 1
-        print(f"Employee {emp_name} is done with tasks"
-              f"({task_done}/{num_task}):")
-        for task_ in tasks:
-            if task_["completed"] is True:
-                print(f"\t {task_['title']}")
-    else:
-        print("No ID provided")
+        emp_usrname = user.get('username')
+        csv_file = f"{user_id}.csv"
+        with open(csv_file, 'w', encoding="utf8") as file:
+            for task in tasks:
+                file.write('"{}","{}","{}","{}"\n'
+                           .format(user_id, emp_usrname,
+                                   task['completed'], task['title']))
 
 
 if __name__ == "__main__":
